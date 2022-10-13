0cc8836054239006340dbd45d49a00eab0ca97f3
Dmitriy <rubagelen@gmail.com>
2022-10-13 23:03:40 +0300

    add task014

---
+++ task014/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая задаёт массив из 8 случайных элементов
+// и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 7, 8
+
+Show(Complete(8));
+
+int[] Complete(int Length)
+{
+    int[] array = new int[Length];
+    for (int i = 0; i <= array.Length - 1; i++)
+    {
+        int b = new Random().Next(1, 100);
+        array[i] = b;
+    }
+    return array;
+}
+void Show(int[] array)
+{
+    for (int i = 0; i <= array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}\t");
+    }
+}
