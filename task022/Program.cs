e38d20010cc813ab3baf99a057ae58d79944d281
Dmitriy <rubagelen@gmail.com>
2022-10-27 22:05:02 +0300

    add task022

---
+++ task022/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.WriteLine("Введите размер массива:  ");
+int N = int.Parse(Console.ReadLine());
+int M = int.Parse(Console.ReadLine());
+int[,] array = new int [N,M];
+Random rnd = new Random();
+
+int[] summ = new int[M];
+
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < M; j++)
+    {
+        array[i,j] = rnd.Next(0,10);
+    }
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < M; j++)
+    {
+        Console.Write($"{array[i,j]}\t");
+        summ[j]+=array[i,j];
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+foreach (double elem in summ)
+{
+    Console.WriteLine($" {(elem/N):f2}");
+}
+
